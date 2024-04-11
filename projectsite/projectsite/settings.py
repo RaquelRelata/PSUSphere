b1dd8ba556369fc7fba3772c1d4748d32e8e074c
RaquelRelata <201980007@psu.palawan.edu.ph>
2024-04-11 11:33:55 +0800

    modify allowed host

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1oez#=1bw%h$&pww!(g3g+p-_il_4_(&42)$s&=8h25s2-xw&q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
