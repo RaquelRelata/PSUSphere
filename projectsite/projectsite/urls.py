24f1f16cac27971ee4dff472e20a2fe9bd09f9ad
RaquelRelata <201980007@psu.palawan.edu.ph>
2024-04-23 10:17:24 +0800

    added changes

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
 from django.urls import path
 from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
-from studentorg import views
+from studentorg.views import OrgMembersList, OrgMembersCreateView, OrgMembersUpdateView, OrgMembersDeleteView
+from .views import FormsView 
+
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.HomePageView.as_view(), name='home'),
-   path('organization_list/', OrganizationList.as_view(), name='organization-list'),
-   path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
-   path('organization_list/<pk>',OrganizationUpdateView.as_view(), name='organization-update'),
-   path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
-
-
+    path('', HomePageView.as_view(), name='home'),
+    path('organization_list/', OrganizationList.as_view(), name='organization-list'),
+    path('organization_list/add/', OrganizationCreateView.as_view(), name='organization-add'),  # Added trailing slash
+    path('organization_list/<int:pk>/', OrganizationUpdateView.as_view(), name='organization-update'),
+    path('organization_list/<int:pk>/delete/', OrganizationDeleteView.as_view(), name='organization-delete'),  # Added trailing slash
+    path('orgmembers/', OrgMembersList.as_view(), name='orgmember-list'),
+    path('orgmembers/add/', OrgMembersCreateView.as_view(), name='orgmember-add'),
+    path('orgmembers/<int:pk>/edit/', OrgMembersUpdateView.as_view(), name='orgmember-edit'),
+    path('orgmembers/<int:pk>/delete/', OrgMembersDeleteView.as_view(), name='orgmember-delete'),
+    path('forms/', FormsView.as_view(), name='forms'),  # Use the FormsView to handle the forms.html page
+    # other urls
 ]
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from studentorg.models import Organization
-from studentorg.forms import OrganizationForm
+from studentorg.models import Organization, OrgMember  # Import OrgMembers
+from studentorg.forms import OrganizationForm, OrgMembersForm  # Import OrgMembersForm
 from django.urls import reverse_lazy
+
+# Define your views here...
+
 
 class HomePageView(ListView):
     model = Organization
@@ -34,3 +37,27 @@ class OrganizationDeleteView(DeleteView):
     model = Organization
     template_name = 'org_del.html'
     success_url = reverse_lazy('organization-list')
+
+
+class OrgMembersList(ListView):
+    model = OrgMember
+    context_object_name = 'OrgMember'
+    template_name = 'org_list.html'
+    paginate_by = 5
+
+class OrgMembersCreateView(CreateView):
+    model = OrgMember
+    form_class = OrgMembersForm
+    template_name = 'org_add.html'
+    success_url = reverse_lazy('orgmember-list')
+
+class OrgMembersUpdateView(UpdateView):
+    model = OrgMember
+    form_class = OrgMembersForm
+    template_name = 'org_edit.html'
+    success_url = reverse_lazy('orgmember-list')
+
+class OrgMembersDeleteView(DeleteView):
+    model = OrgMember
+    template_name = 'org_del.html'
+    success_url = reverse_lazy('orgmember-list')
